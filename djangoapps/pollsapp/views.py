40a6878564079137634f10e8599bde64e18d9666
Ntwali-Josue <kntwalijos@gmail.com>
2021-06-20 07:34:20 +0200

    create first poll app

---
+++ djangoapps/pollsapp/views.py
@@ -0,0 +1,7 @@
+from django.http.response import HttpResponseNotAllowed
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse('Hi Josh')
