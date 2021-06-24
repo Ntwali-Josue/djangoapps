c44d334fbc7f2dc22bb9c428aa3a667a917f1104
Ntwali-Josue <kntwalijos@gmail.com>
2021-06-24 05:28:42 +0200

    create models

--- djangoapps/djangoapps/settings.py
+++ djangoapps/djangoapps/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'django.contrib.PollsappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'pollsapp',
 ]
 
 MIDDLEWARE = [
